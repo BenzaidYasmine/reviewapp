470b83220006133c311c62614f04b337e698d0e5
benzaidyasmine <benzaid_yasmine@hotmail.fr>
2023-11-12 03:38:03 +0100

    Initialisation  projet

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var section = document.querySelector("section");
+
+var requestURL =
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json";
+
+  var request = new XMLHttpRequest();
